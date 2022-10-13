4b69a917d8db3e1b79d68303b9eeaddfd74def1a
Tim Perry <181785897+kriztalz@users.noreply.github.com>
2022-10-13 19:34:02 +0200

    Fully calculate the base JA3 TLS fingerprint data
    This doesn't yet hash the data - but it does successfully parse every
    detail we need in the fingerprint and calculate the correct values, and
    the raw format is more useful for analysis in many cases.

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -2,10 +2,12 @@ import * as net from 'net';
 import * as tls from 'tls';
 import { makeDestroyable, DestroyableServer } from 'destroyable-server';
 
+import { expect } from 'chai';
 import { getDeferred } from './test-util';
 
 import { getTlsFingerprint } from '../src/index';
-import { expect } from 'chai';
+
+const nodeMajorVersion = parseInt(process.version.slice(1).split('.')[0], 10);
 
 describe("Read-TLS-Fingerprint", () => {
 
@@ -22,16 +24,40 @@ describe("Read-TLS-Fingerprint", () => {
         let incomingSocketPromise = getDeferred<net.Socket>();
         server.on('connection', (socket) => incomingSocketPromise.resolve(socket));
 
-        console.log(server.address());
         const port = (server.address() as net.AddressInfo).port;
         tls.connect({
-            host: '127.0.0.1',
+            host: 'localhost',
             port
         }).on('error', () => {}); // Socket will fail, since server never responds, that's OK
 
         const incomingSocket = await incomingSocketPromise;
         const fingerprint = await getTlsFingerprint(incomingSocket);
 
-        expect(fingerprint).to.equal('22'); // Basic WIP test: yes, this is TLS
+        const [
+            tlsVersion,
+            ciphers,
+            extension,
+            groups,
+            curveFormats
+        ] = fingerprint;
+
+        expect(tlsVersion).to.equal(771); // TLS 1.2 - now set even for TLS 1.3 for backward compat
+        expect(ciphers.slice(0, 3)).to.deep.equal([4866, 4867, 4865]);
+        expect(extension).to.deep.equal([
+            11,
+            10,
+            35,
+            22,
+            23,
+            13,
+            43,
+            45,
+            51
+        ]);
+        expect(groups).to.deep.equal([
+            29, 23, 30, 25, 24,
+            ...(nodeMajorVersion >= 17 ? [256, 257, 258, 259, 260] : [])
+        ]);
+        expect(curveFormats).to.deep.equal([0, 1, 2]);
     });
 });
