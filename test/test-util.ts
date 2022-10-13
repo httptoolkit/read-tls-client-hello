686f429e5fd09829e0d3532e4f42121e29be6246
Tim Perry <181785897+kriztalz@users.noreply.github.com>
2022-10-13 20:18:51 +0200

    Remove unused certificate fixtures

--- test/test-util.ts
+++ test/test-util.ts
@@ -1,8 +1,3 @@
-import * as fs from 'fs';
-
-export const testKey = fs.readFileSync(__dirname + '/fixtures/server.key');
-export const testCert = fs.readFileSync(__dirname + '/fixtures/server.crt');
-
 export type Deferred<T> = Promise<T> & {
     resolve(value: T): void,
     reject(e: Error): void
